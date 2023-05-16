906ad589f99b01540da9dab775c2309839929538
Felipe Falquetti <122287535+felipefalq@users.noreply.github.com>
2023-05-16 15:01:24 -0300

    Exercícios básicos

---
+++ Exercicios/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Course
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -> ex00 <-
+            //Console.WriteLine("Entre com seu nome completo: ");
+            //string fullName = Console.ReadLine();
+
+            //Console.WriteLine("Digite quantos quartos tem em sua casa: ");
+            //int bedRooms = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Entre com o preço de um produto: ");
+            //double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Console.WriteLine("Insira seu último nome, sua idade e altura: ");
+            //string[] v = Console.ReadLine().Split(' ');
+
+            //string lastName = v[0];
+            //string age = v[1];
+            //double height = double.Parse(v[2], CultureInfo.InvariantCulture);
+
+            //Console.WriteLine(fullName);
+            //Console.WriteLine(bedRooms);
+            //Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            //Console.WriteLine(lastName);
+            //Console.WriteLine(age);
+            //Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            /////////////////////////////////////////////////
+            
+
+
+            // -> ex01 <-
+            //float num1 = float.Parse(Console.ReadLine());
+            //float num2 = float.Parse(Console.ReadLine());
+
+            //float soma = num1 + num2;
+
+            //Console.WriteLine("SOMA = " + soma);
+
+
+            /////////////////////////////////////////////////
+
+
+            // -> ex02 <-
+            //Console.WriteLine("Digite o valor do raio: ");
+            //float raio = float.Parse(Console.ReadLine());
+
+            //float area = (float)(3.14 * raio * raio);
+            //Console.WriteLine(area);
+
+
+            /////////////////////////////////////////////////
+
+
+            // -> ex03 <-
+            //Console.WriteLine("Digite o valor de a: ");
+            //float a = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor de b: ");
+            //float b = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor de c: ");
+            //float c = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor de d: ");
+            //float d = float.Parse(Console.ReadLine());
+
+            //float diferenca = a * b - c * d;
+
+            //Console.WriteLine("Diferenca = " + diferenca);
+
+
+            /////////////////////////////////////////////////
+
+
+            // -> ex04 <-
+            //Console.WriteLine("Digite seu ID de funcionário: ");
+            //int id = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite as suas horas trabalhadas: ");
+            //float horas = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite quanto você ganha por hora: ");
+            //double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //float salario = (float)((double)horas * valorHora);
+
+            //Console.WriteLine("Seu ID é: " + id);
+            //Console.WriteLine("Seu salário é: " + salario.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            /////////////////////////////////////////////////
+
+
+            // -> ex05 <-
+            //int cod1 = int.Parse(Console.ReadLine());
+            //int qtd1 = int.Parse(Console.ReadLine());
+            //double preco1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            //int cod2 = int.Parse(Console.ReadLine());
+            //int qtd2 = int.Parse(Console.ReadLine());
+            //double preco2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //double total = preco1 * qtd1 + preco2 * qtd2;
+            //Console.WriteLine("Valor total = " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            /////////////////////////////////////////////////
+
+
+            // -> ex06 <-
+            //double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //double triangulo = a * c / 2.0;
+            //double circulo = 3.14 * c * c;
+            //double trapezio = (a + b) / 2.0 * c;
+            //double quadrado = b * b;
+            //double retangulo = a * b;
+
+            //Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3"), CultureInfo.InvariantCulture);
+            //Console.WriteLine("CIRCULO: " + circulo.ToString("F3"), CultureInfo.InvariantCulture);
+            //Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3"), CultureInfo.InvariantCulture);
+            //Console.WriteLine("QUADRADO: " + quadrado.ToString("F3"), CultureInfo.InvariantCulture);
+            //Console.WriteLine("RETANGULO: " + retangulo.ToString("F3"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
